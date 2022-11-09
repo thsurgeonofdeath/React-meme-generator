8040b3481c09999d36b0996f3d03e2af850ddad1
thsurgeonofdeath <salahek25@gmail.com>
2022-11-09 20:03:49 +0100

    generate meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,24 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+    
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -16,10 +31,13 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={memeImage} className="meme--image" />
+            
         </main>
     )
 }
